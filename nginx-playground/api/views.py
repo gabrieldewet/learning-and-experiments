ea06de42484f10f3182a13d34dbfd5dac19bb33a
gabrieldewet <giebels.dewet@gmail.com>
2025-02-16 22:36:22 +0100

    populate api views

--- nginx-playground/api/views.py
+++ nginx-playground/api/views.py
@@ -30,9 +30,14 @@ from .serializers import UpdateStatusSerializer, WatchlistItemSerializer
 @api_view("POST", "GET")
 def get_update_movie(request: Request):
     if request.method == "GET":
-        ...
+        movies = WatchlistItem.objects.filter(watched=False).order_by("-created_at")
+        for movie in movies:
+            serializer = WatchlistItemSerializer(movie)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "POST":
-        ...
+        input_serializer = UpdateStatusSerializer(data=request.data, partial=True)
+        if input_serializer.is_valid():
+            ...
 
     return Response(data={"error": f"Invalid HTTP method {request.method}"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
