7c4a93749ca9781757dfa316f21cfa6f7297ff15
gabrieldewet <giebels.dewet@gmail.com>
2025-02-17 22:18:16 +0100

    another day another dollar

--- nginx-playground/api/views.py
+++ nginx-playground/api/views.py
@@ -6,26 +6,6 @@ from rest_framework.response import Response
 from .models import WatchlistItem
 from .serializers import UpdateStatusSerializer, WatchlistItemSerializer
 
-# TODO: Add these back in at some point to test generics, for now it's not readable if
-# you don't know wtf you're doing
-# class WatchlistItemRetrieve(generics.RetrieveAPIView):
-#     queryset = WatchlistItem.objects.filter(watched=False).order_by("-created_at").get()
-#     serializer_class = WatchlistItemSerializer
-
-
-# class WatchlistItemDetail(generics.RetrieveUpdateAPIView):
-#     queryset = WatchlistItem.objects.all()
-#     serializer_class = UpdateStatusSerializer
-
-#     def post(self, request, *args, **kwargs):
-#         instance = self.get_object()
-
-#         serializer = self.get_serializer(instance, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 @api_view("POST", "GET")
 def get_update_movie(request: Request):
